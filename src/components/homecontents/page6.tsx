91ba8daa996cc22575a9b3d1b6b43df5de99b2ee
atsuhiro <ishikawaatsuhito1205@gmail.com>
2025-04-06 16:57:23 +0900

    created_home

--- src/components/homecontents/page6.tsx
+++ src/components/homecontents/page6.tsx
@@ -5,12 +5,12 @@ export default function page6() {
             <div className="w-full h-[100px]"></div>
             <div className="w-full h-[calc(100%-100px)] space-y-[5px] px-4 sm:px-8">
                 <p className="text-[40px] sm:text-[50px]">Work Style</p>
-                <p className="text-[15px] sm:text-[22px] leading-relaxed">
-                    I aim to build strong relationships with clients through open communication and shared goals. I value:<br />
-                    ・Clear expectations<br />
-                    ・Proactive updates<br />
-                    ・Flexible yet accountable work styles<br />
-                    Whether you&apos;re looking for quick implementation support or a long-term product partner, I&apos; ready to contribute.
+                <p className="text-[22px]">
+                    I aim to build strong relationships with clients through open communication and shared goals. I value:<br/>
+                    ・Clear expectations<br/>
+                    ・Proactive updates<br/>
+                    ・Flexible yet accountable work styles<br/>
+                    Whether you&apos;re looking for quick implementation support or a long-term product partner, I&apos;m ready to contribute.
                 </p>
             </div>
         </>
